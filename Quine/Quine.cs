be8c5479b65033a7b272be6adce8cd28c4af0dc7
Cooper Saye <coopersaye@gmail.com>
2020-10-29 23:43:52 -0700

    Create C# quine program.

---
+++ Quine/Quine.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Quine
+{
+    static void Main()
+    {
+        string s =
+        @"
+using System;
+
+class Quine
+{{
+    static void Main()
+    {{
+        string s =
+        @{0}{1}{0};
+        
+        Console.WriteLine(s, '{0}', s);
+    }}
+}}";
+        
+        Console.WriteLine(s, '"', s);
+    }
+}

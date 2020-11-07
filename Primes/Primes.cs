1cce3fe782c3e04435831a1b6f1d963b155642cd
Cooper Saye <coopersaye@gmail.com>
2020-11-06 22:06:53 -0800

    Create C# primes program.

---
+++ Primes/Primes.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Primes
+{
+    static void Main()
+    {
+        for (int i = 2; i < 100; i++)
+        {
+            bool isPrime = true;
+            int upperBound = (int)Math.Sqrt(i);
+
+            for (int j = 2; j <= upperBound; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) Console.WriteLine(i);
+        }
+    }
+}

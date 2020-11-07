18b93bd8205dac8af29403e27d6869a3381da567
Cooper Saye <coopersaye@gmail.com>
2020-11-06 22:10:14 -0800

    Create C++ primes program.

---
+++ Primes/Primes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    for (int i = 2; i < 100; i++)
+    {
+        bool isPrime = true;
+        int upperBound = (int)std::sqrt(i);
+
+        for (int j = 2; j <= upperBound; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) std::cout << i << "\n";
+    }
+}
